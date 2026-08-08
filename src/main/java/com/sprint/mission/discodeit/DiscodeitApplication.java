f7cdd087e71b5a5c988c6a3925dfd4f93d68a1fe
Suyoung <sueyoungkim69@gmail.com>
2026-08-08 19:26:02 +0900

    기본 요구사항

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -6,29 +6,19 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import com.sprint.mission.discodeit.repository.MessageRepository;
-import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
-import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
-import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.basic.BasicChannelService;
-import com.sprint.mission.discodeit.service.basic.BasicMessageService;
-import com.sprint.mission.discodeit.service.basic.BasicUserService;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 	static User setupUser(UserService userService) {
-		User user = userService.create("woody", "woody@codeit.com", "woody1234");
-		return user;
+		return userService.create("woody", "woody@codeit.com", "woody1234");
 	}
 
 	static Channel setupChannel(ChannelService channelService) {
-		Channel channel = channelService.create(ChannelType.PUBLIC, "공지", "공지 채널입니다.");
-		return channel;
+		return channelService.create(ChannelType.PUBLIC, "공지", "공지 채널입니다.");
 	}
 
 	static void messageCreateTest(MessageService messageService, Channel channel, User author) {
@@ -37,24 +27,14 @@ public class DiscodeitApplication {
 	}
 
 	public static void main(String[] args) {
-		// 레포지토리 초기화
-		UserRepository userRepository = new FileUserRepository();
-		ChannelRepository channelRepository = new FileChannelRepository();
-		MessageRepository messageRepository = new FileMessageRepository();
+		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
 
-		// 서비스 초기화
-		UserService userService = new BasicUserService(userRepository);
-		ChannelService channelService = new BasicChannelService(channelRepository);
-		MessageService messageService = new BasicMessageService(messageRepository, channelRepository, userRepository);
+		UserService userService = context.getBean(UserService.class);
+		ChannelService channelService = context.getBean(ChannelService.class);
+		MessageService messageService = context.getBean(MessageService.class);
 
-		// 셋업
 		User user = setupUser(userService);
 		Channel channel = setupChannel(channelService);
-		// 테스트
 		messageCreateTest(messageService, channel, user);
 	}
-
-	/*public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
-	}*/
 }
